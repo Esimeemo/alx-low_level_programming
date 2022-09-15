149ad4d5722f0433e5135f115adbad79f1e42d09
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-14 20:53:14 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ *add - adds to ints
+ *
+ *@a: one integer
+ *
+ *@b: another integer
+ *
+ *Return: an int = a+b
+ */
+int add(int a, int b)
+
+{
+return (a + b);
+}
