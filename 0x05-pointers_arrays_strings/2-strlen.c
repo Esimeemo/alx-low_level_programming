cc8eca7bd74e7033a6a17fa479b731d3a8f67ee6
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-20 07:31:05 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _strlen - a function that returns the lenght of a string
+ * @s: the input string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length != '\0' ; length++)
+		;
+	return (legth);
+}
