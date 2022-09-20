a5c605693fdd881dabffa31e6e346bac6961513b
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-20 00:26:57 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - writes code that resets value to 98
+ * @n: it points to an int and update the value to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
