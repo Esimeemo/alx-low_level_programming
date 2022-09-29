2f9caa1672743583abccd83b756e894c3c31af11
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-28 22:58:38 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,3 +29,26 @@ void check(char *s, int start, int end, int *flag)
 	{
 		if (s[start] == s[end])
 			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
+
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum == _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
