7425986e4678a3dc76299701d43251b5d03d37b0
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-30 11:42:17 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: argument constant
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		print("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
