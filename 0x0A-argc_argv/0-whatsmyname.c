cae88dfc94d5efca6f788ea718a82b30c4434882
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-30 10:18:33 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,16 @@
 
 /**
  * main - a program that prints its name
- * @argc: number of things you entered when running this programme in the command line
- * @argv[]: array that holds the value of the things that were entered
+ * @argc: number of things you entered when running this programme
+ * @argv: array that holds the value of the things that were entered
  *
- * return: 0
+ * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-
 	printf("%s\n", argv[0]);
-	return 0;
+
+	return (0);
 }
