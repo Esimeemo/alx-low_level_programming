25550e2058ea1474b1740d5c746af8918273fbeb
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-09-30 11:06:45 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,8 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	{
-	printf("Number of arguments are: %d\n", argc - 1);
-	}
+	printf("%ii\n", argc - 1);
+
 	return (0);
 }
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,2 @@
+_putchar(char c);
+int check_num(char *str);
