a551bab9110c1016ba974ab8dbc1bd91c662697c
Esimeemo <111182867+Esimeemo@users.noreply.github.com>
2022-10-01 22:11:05 -0700

    task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the lenght of a string
+ * @s: the input string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*(s + length)  != '\0')
+		length++;
+
+	return (length);
+}
